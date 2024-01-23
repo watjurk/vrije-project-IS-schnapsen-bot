c6de66c5b6dee09ed85fe19c40e57ebd28f31527
Jules Padova <62508133+JulesPad@users.noreply.github.com>
2024-01-23 19:37:25 +0100

    update expert varibale name

--- src/bot/llm/llm_engine.py
+++ src/bot/llm/llm_engine.py
@@ -6,11 +6,16 @@ from os import path
 MODEL_TYPE = "openhermes"  # Update this for your model
 
 CURRENT_FOLDER = path.dirname(path.abspath(__file__))
+MODEL_TYPE = "openhermes"  # Update this for your model
+
+# read the master prompt from a file
 PLAYING_PROMPT = open(path.join(CURRENT_FOLDER, "playing_prompt.txt")).read()
 
+# read the expert prompt from a file
+EXPERT_PROMPT = open(path.join(CURRENT_FOLDER, "expert_prompt.txt")).read()
 
 def expert(history):
-    full_expert_prompt = expert_prompt + history
+    full_expert_prompt = EXPERT_PROMPT + history
     r = requests.post(
         "http://localhost:11434/api/generate",
         json={
@@ -32,6 +37,7 @@ def expert(history):
 
         if body.get("done", False):
             return output
+
 
 
 def generate(game_representation: str, feedback: Optional[str] = None) -> str:
