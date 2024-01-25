ccf9414e43f258136d7e8e89cd4989031d7c8bfc
Jules Padova <62508133+JulesPad@users.noreply.github.com>
2024-01-25 17:59:40 +0100

    commented rag code lines for temporay test on server

--- src/bot/llm/llm_engine.py
+++ src/bot/llm/llm_engine.py
@@ -3,7 +3,7 @@ from os import path
 from typing import Optional
 
 import requests
-from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
+#from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
 
 MODEL_TYPE = "openhermes"
 
@@ -12,11 +12,11 @@ CURRENT_FOLDER = path.dirname(path.abspath(__file__))
 PLAYING_PROMPT = open(path.join(CURRENT_FOLDER, "playing_prompt_v3.txt")).read()
 EXPERT_PROMPT = open(path.join(CURRENT_FOLDER, "expert_prompt_v3.txt")).read()
 
-rag_documents = SimpleDirectoryReader(path.join(CURRENT_FOLDER, "rag_data")).load_data()
+#rag_documents = SimpleDirectoryReader(path.join(CURRENT_FOLDER, "rag_data")).load_data()
 
-service_context = ServiceContext.from_defaults(llm=None, embed_model="local")
-rag_index = VectorStoreIndex.from_documents(rag_documents, service_context=service_context)
-rag_retriever_engine = rag_index.as_retriever()
+#service_context = ServiceContext.from_defaults(llm=None, embed_model="local")
+#rag_index = VectorStoreIndex.from_documents(rag_documents, service_context=service_context)
+#rag_retriever_engine = rag_index.as_retriever()
 
 
 def llm_expert(game_history: str, game_representation_without_history: str):
@@ -32,21 +32,21 @@ def llm_player(
     history = game_history
     expert_advice = llm_expert(history, game_representation_without_history)
 
-    rag_response = ""
-    rag_responses = rag_retriever_engine.retrieve(expert_advice)
-    for response in rag_responses:
-        rag_response += f"{response.get_text()}\n"
+    #rag_response = ""
+    #rag_responses = rag_retriever_engine.retrieve(expert_advice)
+    #for response in rag_responses:
+        #rag_response += f"{response.get_text()}\n"
     # Remove tailing enter
-    rag_response = rag_response[:-1]
+    #rag_response = rag_response[:-1]
 
     full_prompt = f"{PLAYING_PROMPT}"
     full_prompt += f"\n\nSTATE:\n{game_representation_without_history}"
     full_prompt += f"\n\nEXPERT ADVICE:\n{expert_advice}"
-    full_prompt += f"\n\nBOOK ADVICE:\n{rag_response}"
+    #full_prompt += f"\n\nBOOK ADVICE:\n{rag_response}"
     if feedback is not None:
         full_prompt += f"\n\nFEEDBACK:\n{feedback}"
     
-    print(full_prompt)
+    #print(full_prompt)
 
     return api_generate(full_prompt)
 
