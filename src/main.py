00f51db66f4d15973bfe189f6071f202eff8880b
Wiktor Jurkiewicz <watjurk@gmail.com>
2024-01-25 14:35:29 +0100

    improved cli tournament functionality

--- src/main.py
+++ src/main.py
@@ -1,76 +1,34 @@
-import click
-
 import random
 import time
 
-import schnapsen.game
+import click
+
 import schnapsen.bots
 
-from bot import LLMBot
+from tournament import run_tournament_against_bot
 
 
 @click.command()
-@click.option("-n", "--number", default=20, help="Number of games to play against each bot.")
-def run_tournament(number):
-    number_of_games_against_each_bot = number
-    engine = schnapsen.game.SchnapsenGamePlayEngine()
+@click.option("number_of_games", "-n", "--number", default=20, help="Number of games to play.")
+@click.option("csv_path", "-c", "--csv", help="CSV file path.", required=True)
+@click.option(
+    "opponent_bot_id",
+    "-b",
+    "--bot",
+    type=click.Choice(["Rdeep", "Rand"], case_sensitive=False),
+    help="Bot to play against.",
+    required=True,
+)
+def run_tournament(number_of_games, csv_path, opponent_bot_id):
+    if opponent_bot_id == "Rdeep":
+        opponent_bot = schnapsen.bots.RdeepBot(
+            num_samples=2, depth=4, rand=random.Random(time.time()), name="rdeep"
+        )
 
-    our_bot = LLMBot(name="llmbot")
-    # our_bot = schnapsen.bots.RandBot(rand=random.Random(time.time()), name="our_randbot")
-    # out_bot_id = str(our_bot)
+    if opponent_bot_id == "Rand":
+        opponent_bot = schnapsen.bots.RandBot(random.Random(time.time()), name="rand")
 
-    bot1 = schnapsen.bots.RandBot(rand=random.Random(time.time()), name="randbot")
-    bot2 = schnapsen.bots.RdeepBot(num_samples=2, depth=4, rand=random.Random(time.time()), name="rdeepbot")
-    bots_to_play_against = [bot1, bot2]
-
-    # all_bots_in_tournament = [our_bot]
-    # all_bots_in_tournament.extend(bots_to_play_against)
-
-    total_games = number_of_games_against_each_bot * len(bots_to_play_against)
-    print(f"---- playing {total_games} games ----")
-    print(f"---- {number_of_games_against_each_bot} games against each bot ----")
-
-    our_total_win_count = 0
-
-    for bot in bots_to_play_against:
-        print()
-        print(f"-- playing against {bot} --")
-        our_win_count, opponent_win_count = 0, 0
-        our_game_points, opponent_game_points = 0, 0
-        our_score, opponent_score = 0, 0
-
-        for game_number in range(number_of_games_against_each_bot):
-            print(f"playing game number {game_number+1 :<2}: ", end="")
-            is_our_bot_starting = random.choice([True, False])
-            if is_our_bot_starting:
-                first_bot = our_bot
-                second_bot = bot
-            else:
-                first_bot = bot
-                second_bot = our_bot
-
-            winner_bot, game_points, score = engine.play_game(first_bot, second_bot, random.Random(time.time()))
-            print(f"{str(winner_bot):<13} won the game, with score: {score.direct_points}, obtained: {game_points} game points")
-
-            if winner_bot is our_bot:
-                our_win_count += 1
-                our_game_points += game_points
-                our_score += score.direct_points
-            else:
-                opponent_win_count += 1
-                opponent_game_points += game_points
-                opponent_score += score.direct_points
-
-        our_total_win_count += our_win_count
-        print()
-        print(f"-- stats against {bot} --")
-        print(f"-- we won {(our_win_count/number_of_games_against_each_bot)*100:.2f}% of the games")
-        # print(f"-- we won {our_win_count} out of {number_of_games_against_each_bot} games --")
-        print(f"-- our game points: {our_game_points}, opponent game points: {opponent_game_points} --")
-        print(f"-- our score: {our_score}, opponent score: {opponent_score} score --")
-
-    print()
-    print(f"---- in total we won {(our_total_win_count/total_games)*100:.2f}% of the games ----")
+    run_tournament_against_bot(csv_path, number_of_games, opponent_bot)
 
 
 if __name__ == "__main__":
---
+++ src/tournament.py
@@ -0,0 +1,91 @@
+import csv
+import os
+import random
+import time
+
+import schnapsen.game
+import schnapsen.game
+
+from bot import LLMBot
+
+
+class GameStats:
+    did_our_bot_win: bool
+    opponent_bot_id: str
+    game_points_obtained: int
+    score_obtained: int
+
+
+def run_tournament_against_bot(
+    csv_path: str,
+    number_of_games: int,
+    opponent_bot: schnapsen.game.Bot,
+) -> None:
+    write_header = True
+    if os.path.isfile(csv_path):
+        write_header = False
+
+    file = open(csv_path, "a")
+    csv_writer = csv.writer(file)
+    if write_header:
+        csv_writer.writerow(
+            ["did_our_bot_win", "opponent_bot_id", "game_points_obtained", "score_obtained"]
+        )
+
+    engine = schnapsen.game.SchnapsenGamePlayEngine()
+
+    our_bot = LLMBot(name="llmbot")
+    opponent_bot_id = str(opponent_bot)
+
+    print(f"-- playing {number_of_games} games against: {opponent_bot} --")
+    our_win_count, opponent_win_count = 0, 0
+    our_game_points, opponent_game_points = 0, 0
+    our_score, opponent_score = 0, 0
+
+    for game_number in range(number_of_games):
+        print(f"playing game number {game_number+1 :<2}: ", end="")
+        is_our_bot_starting = game_number % 2 == 0
+        if is_our_bot_starting:
+            first_bot = our_bot
+            second_bot = opponent_bot
+        else:
+            first_bot = opponent_bot
+            second_bot = our_bot
+
+        winner_bot, game_points, score = engine.play_game(
+            first_bot, second_bot, random.Random(time.time())
+        )
+        print(
+            f"{str(winner_bot):<13} won the game, with score: {score.direct_points}, obtained: {game_points} game points"
+        )
+
+        game_stats = GameStats()
+        game_stats.did_our_bot_win = winner_bot is our_bot
+        game_stats.opponent_bot_id = opponent_bot_id
+
+        game_stats.game_points_obtained = game_points
+        game_stats.score_obtained = score.direct_points
+        csv_writer.writerow(
+            [
+                game_stats.did_our_bot_win,
+                game_stats.opponent_bot_id,
+                game_stats.game_points_obtained,
+                game_stats.score_obtained,
+            ]
+        )
+
+        if winner_bot is our_bot:
+            our_win_count += 1
+            our_game_points += game_points
+            our_score += score.direct_points
+        else:
+            opponent_win_count += 1
+            opponent_game_points += game_points
+            opponent_score += score.direct_points
+
+    print(
+        f"-- our bot won {(our_win_count/number_of_games)*100:.2f}% of the games against: {opponent_bot}"
+    )
+
+    file.flush()
+    file.close()
