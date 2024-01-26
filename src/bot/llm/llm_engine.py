49d860095aa5f12ea4d37a4c1098359c3c458e75
Jules Padova <62508133+JulesPad@users.noreply.github.com>
2024-01-26 01:14:35 +0100

    created double expert system with RAG running on mixture of expert models

--- src/bot/llm/llm_engine.py
+++ src/bot/llm/llm_engine.py
@@ -3,26 +3,31 @@ from os import path
 from typing import Optional
 
 import requests
-#from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
+from llama_index import ServiceContext, SimpleDirectoryReader, VectorStoreIndex
 
-MODEL_TYPE = "mixtral"
+#MODEL_TYPE = "mixtral:8x7b-instruct-v0.1-q5_0"
+MODEL_TYPE = "openhermes"
 
 
 CURRENT_FOLDER = path.dirname(path.abspath(__file__))
-PLAYING_PROMPT = open(path.join(CURRENT_FOLDER, "playing_prompt_v3.txt")).read()
-EXPERT_PROMPT = open(path.join(CURRENT_FOLDER, "expert_prompt_v3.txt")).read()
+PLAYING_PROMPT = open(path.join(CURRENT_FOLDER, "playing_prompt_v4.txt")).read()
+EXPERT_PROMPT = open(path.join(CURRENT_FOLDER, "expert_prompt_v4.txt")).read()
+CONTEXT_EXPERT_PROMPT = open(path.join(CURRENT_FOLDER, "expert_and_rag.txt")).read()
+rag_documents = SimpleDirectoryReader(path.join(CURRENT_FOLDER, "rag_data")).load_data()
 
-#rag_documents = SimpleDirectoryReader(path.join(CURRENT_FOLDER, "rag_data")).load_data()
 
-#service_context = ServiceContext.from_defaults(llm=None, embed_model="local")
-#rag_index = VectorStoreIndex.from_documents(rag_documents, service_context=service_context)
+service_context = ServiceContext.from_defaults(llm=None, embed_model="local")
+rag_index = VectorStoreIndex.from_documents(rag_documents, service_context=service_context)
 #rag_retriever_engine = rag_index.as_retriever()
-
+rag_retriever_engine = rag_index.as_retriever(similarity_top_k=1)
 
 def llm_expert(game_history: str, game_representation_without_history: str):
     full_expert_prompt = f"{EXPERT_PROMPT}\n\nHISTORY:\n{game_history}\n\nSTATE:\n{game_representation_without_history}"
     return api_generate(full_expert_prompt)
 
+def context_expert(expert_advice, rag_response, game_representation_without_history):
+    context_expert_prompt = f"{CONTEXT_EXPERT_PROMPT}\n\nEXPERT ADVICE:\n{expert_advice}\n\nBOOK CONTEXT:\n\n{rag_response}\n\nCURRENT STATE:\n{game_representation_without_history}"
+    return api_generate(context_expert_prompt)
 
 def llm_player(
     game_representation_without_history: str,
@@ -32,21 +37,28 @@ def llm_player(
     history = game_history
     expert_advice = llm_expert(history, game_representation_without_history)
 
-    #rag_response = ""
-    #rag_responses = rag_retriever_engine.retrieve(expert_advice)
-    #for response in rag_responses:
-        #rag_response += f"{response.get_text()}\n"
+    rag_response = ""
+    rag_responses = rag_retriever_engine.retrieve(expert_advice)
+    for response in rag_responses:
+        rag_response += f"{response.get_text()}\n"
     # Remove tailing enter
-    #rag_response = rag_response[:-1]
+    rag_response = rag_response[:-1]
+
+    context_expert_advice = context_expert(expert_advice, rag_response, game_representation_without_history)
+
+    
+    print("\n\n")
+    print("FINAL ADVICE")
+    print(context_expert_advice)
+    print("\n\n")
 
     full_prompt = f"{PLAYING_PROMPT}"
     full_prompt += f"\n\nSTATE:\n{game_representation_without_history}"
-    full_prompt += f"\n\nEXPERT ADVICE:\n{expert_advice}"
+    full_prompt += f"\n\nEXPERT ADVICE:\n{context_expert_advice}"
     #full_prompt += f"\n\nBOOK ADVICE:\n{rag_response}"
     if feedback is not None:
         full_prompt += f"\n\nFEEDBACK:\n{feedback}"
     
-    #print(full_prompt)
 
     return api_generate(full_prompt)
 
